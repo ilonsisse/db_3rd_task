fb750b99d3c039873caa3c694842e9ff3d31afba
Evgenii Istomin <ilonsisse@yandex.ru>
2024-07-11 01:51:45 +0300

    changed

--- select.sql
+++ select.sql
@@ -26,34 +26,6 @@ SELECT name AS track_name
 FROM track
 WHERE LOWER(name) LIKE '%мой%' OR LOWER(name) LIKE '%my%';
 
--- Название и продолжительность самого длительного трека
-SELECT name AS track_name, duration
-FROM track
-WHERE duration = (
-    SELECT MAX(duration)
-    FROM track
-);
-
--- Название треков, продолжительность которых не менее 3,5 минут
-SELECT name AS track_name
-FROM track
-WHERE duration >= '00:03:30';
-
--- Названия сборников, вышедших в период с 2018 по 2020 год включительно
-SELECT name AS compilation_name
-FROM compilation
-WHERE release_year BETWEEN 2018 AND 2020;
-
--- Исполнители, чьё имя состоит из одного слова
-SELECT name, surname
-FROM singer
-WHERE nickname NOT LIKE '% %'; 
-
--- Название треков, которые содержат слово «мой» или «my»
-SELECT name AS track_name
-FROM track
-WHERE LOWER(name) LIKE '%мой%' OR LOWER(name) LIKE '%my%';
-
 
 -- Количество исполнителей в каждом жанре
 SELECT g.genre, COUNT(s.singer_nickname) AS count_of_singers
